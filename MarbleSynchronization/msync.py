b4d8251cc6449d5f7d82bb0ed3065e28a2b920b4
Jeffrey Tan <tanjeffreyz02@gmail.com>
2020-08-13 16:36:16 -0700

    fixed the img_points error. implemented rudimentary gaussian noise while talking, will convert to features dot product later

--- MarbleSynchronization/msync.py
+++ MarbleSynchronization/msync.py
@@ -19,17 +19,21 @@ WINDOW_TITLE = 'Facial Landmark Detector'
 IMAGE_RESCALE = 75
 VIDEO_RESCALE = 50
 
-LOW_PASS = 20
+#LOW_PASS = 20
 
+WINDOW_LENGTH = 3
 K_SPEECH = 1.0
-K_EYEBROW = 1.0
+#K_EYEBROW = 1.0
 
-tilt_weight = {'speech_exaggeration_multiplier': 1.0,
-            'resting_lip_spacing': 20
-    }
+#tilt_weight = {'speech_exaggeration_multiplier': 1.0,
+#            'resting_lip_spacing': 20
+#    }
 #tilt
 
 #pan_template
+
+
+
 
 
 #############################
@@ -38,6 +42,7 @@ tilt_weight = {'speech_exaggeration_multiplier': 1.0,
 # Computer Vision Initialization
 detector = dlib.get_frontal_face_detector()     # Get a face detector from the dlib library
 predictor = dlib.shape_predictor('assets/shape_predictor_68_face_landmarks.dat')      # Get a shape predictor based on a trained model from the dlib library
+sliding_window = []
 
 def check_str(value):
     if type(value) != str:
@@ -139,7 +144,14 @@ def main():
         else:
             moving_average = []
             n = 6
-            rot_frame = []
+
+            calibration = []
+            calibrated = False
+            c_n = 30
+            rotation_offsets = (0, 0, 0)
+
+            #profile = Profile("Jeffrey")
+
             while cap.isOpened():           # Main while loop
                 for event in pygame.event.get():        # Pygame
                     if event.type == pygame.QUIT:
@@ -147,64 +159,100 @@ def main():
                         quit()
                 
                 s, frame = cap.read()
-                if s:
-                    frame, landmarks, img_points = detect(frame, mark=True)
-                    frame = rescale_frame(frame, percent=rescale)
+                assert s, 'Failed to read next frame'
+                frame, landmarks, img_points = detect(frame, mark=True)
+                frame = rescale_frame(frame, percent=rescale)
 
-                    if img_points.size != 0:
-                        rotation, translation = pe.estimate_pose(img_points)
+                #print(len(img_points))
+
+                if img_points.size != 0:
+                    rotation, translation = pe.estimate_pose(img_points)
+
+                    if not calibrated:
+                        calibration.append(rotation)
+                        #print(calibration)
+                        if len(calibration) > c_n:
+                            averages = [sum([rot[i] for rot in calibration]) / c_n for i in range(3)]
+                            rotation_offsets = tuple(-avg for avg in averages)
+                            calibrated = True
                     
-                        print(img_points)
-                        #if len(rotation) != 0:
-                        moving_average.append(rotation)
-                        if len(moving_average) > n:
-                            moving_average.pop(0)
-                        #rotation = [sum([rot[i] for rot in moving_average]) / n for i in range(3)] 
-                        rotation = [ewma([rot[i] for rot in moving_average]) for i in range(3)]
-                        if landmarks.size != 0:
-                            rotation[0] += -K_SPEECH * (landmarks[51][1] - landmarks[57][1] + 20)        # Nodding
-                            # rotati
-                        #print(landmarks[51][1] - landmarks[57][1])
-                        #print(landmarks[24][1] - landmarks[44][1])
+                    #print(img_points)
+                    #if len(rotation) != 0:
+                    moving_average.append(rotation)
+                    if len(moving_average) > n:
+                        moving_average.pop(0)
+                    #rotation = [sum([rot[i] for rot in moving_average]) / n for i in range(3)] 
+                    rotation = [ewma([rot[i] for rot in moving_average]) for i in range(3)]
+                    if landmarks.size != 0:
+                        rotation[0] += -K_SPEECH * (landmarks[51][1] - landmarks[57][1] + 20)        # nodding
+                    #print(landmarks[51][1] - landmarks[57][1])
+                    #print(landmarks[24][1] - landmarks[44][1])
 
-                        #converted_rotation = tuple(jbr(rot) for rot in rotation)
-                        converted_rotation = rotation
+                    g_noise = tuple(numpy.random.normal(scale=3.0) for _ in range(3)) if abs(landmarks[51][1] - landmarks[57][1] + 20) > 5 else (0, 0, 0)
 
-                        rot_frame.append((converted_rotation[0], converted_rotation[1]))
-                        if len(rot_frame) > 2:
-                            rot_frame.pop(0)
-                            d_x, d_y = rot_frame[0][0] - rot_frame[1][0], rot_frame[1][1] - rot_frame[0][1]
-                            if abs(d_x) > LOW_PASS or abs(d_y) > LOW_PASS:
-                                converted_rotation = rot_frame[0]
-                                #rot_frame[1] = rot_frame[0]
+                    #converted_rotation = tuple(jbr(rot) for rot in rotation)
+                    converted_rotation = tuple(rotation[i] + rotation_offsets[i] for i in range(3)) 
+
+                    #tilt_weights = get_tilt_weights(profile)
+
+                    #tilt_features = get_tilt_features(profile, rotation, translation, landmarks)
+                    
+                    #log_features_weights(tilt_features, tilt_weights)
+
+                    #tilt = dot_product(tilt_features, tilt_weights)
+                    #pan = 0
+
+                    #rot_frame.append((converted_rotation[0], converted_rotation[1]))
+                    #if len(rot_frame) > 2:
+                    #    rot_frame.pop(0)
+                    #    d_x, d_y = rot_frame[0][0] - rot_frame[1][0], rot_frame[1][1] - rot_frame[0][1]
+                    #    if abs(d_x) > LOW_PASS or abs(d_y) > LOW_PASS:
+                    #        converted_rotation = rot_frame[0]
+                            #rot_frame[1] = rot_frame[0]
                                 
 
-                        #print('timestamp:{3}\npitch:{0}\nyaw:{1}\nroll{2}\n'.format(*converted_rotation, cap.get(cv2.CAP_PROP_POS_MSEC)))
-                        file.write('{0},{1},{2}\n'.format(int(cap.get(cv2.CAP_PROP_POS_MSEC)), converted_rotation[0][0], converted_rotation[1][0]))
+                    #print('timestamp:{3}\npitch:{0}\nyaw:{1}\nroll{2}\n'.format(*converted_rotation, cap.get(cv2.CAP_PROP_POS_MSEC)))
+                    #print(rotation_offsets)
+                    file.write('{0},{1},{2}\n'.format(int(cap.get(cv2.CAP_PROP_POS_MSEC)), converted_rotation[0][0], converted_rotation[1][0]))
 
-                        glPushMatrix()
-                        glRotatef(rotation[0], 1, 0, 0)
-                        glRotatef(rotation[1], 0, -1, 0)
-                        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-                        draw_sphere()
-                        draw_cylinder()
-                        glPopMatrix()
-                        #print('({0}, {1})'.format(marble_renderer.rotate_x, marble_renderer.rotate_y))
-
-                    cv2.imshow(WINDOW_TITLE, frame)
-                    if cv2.waitKey(1) & 0xFF == 27:     # 27 is ASCII for the Esc key on a keyboard
-                        break
+                    #rotate_marble(pan, tilt)
+                    rotate_marble(converted_rotation[0], converted_rotation[1])
                 else:
-                    break
+                    print('Failed to find image points')
 
-                #Cube()
-                #draw_sphere()
-                #draw_cylinder()
+                cv2.imshow(WINDOW_TITLE, frame)
+                if cv2.waitKey(1) & 0xFF == 27:     # 27 is ASCII for the Esc key on a keyboard
+                    break
 
                 pygame.display.flip()
                 pygame.time.wait(10)
     cap.release()
     cv2.destroyAllWindows()
+
+
+#############################
+#          Classes          #
+#############################
+class Profile:
+    def __init__(self, name):
+        if name == "Jaiveer":
+            self.resting_tilt = 32
+            self.resting_lip_distance = 0
+            self.resting_eyebrow_elevation = 43
+            self.weights = {
+                "measured_tilt_degrees": 1,
+                "lip_spacing": 3,
+                "eyebrow_elevation": -5
+            }
+        elif name == "Jeffrey":
+            self.resting_tilt = 20
+            self.resting_lip_distance = 2
+            self.resting_eyebrow_elevation = 32
+            self.weights = {
+                "measured_tilt_degrees": 1,
+                "lip_spacing": 3,
+                "eyebrow_elevation": -3
+            }
 
 
 #############################
@@ -219,6 +267,43 @@ def create_file(file_name, file_type, n=0):
     else:
         return create_file(file_name, file_type, n+1)
 
+#def ewma(feature_data, exceptions):
+#    def avg(data):
+#        alpha = 2 / (len(data) + 1)
+#        if len(data) == 1:
+#            return data[-1]
+#        else:
+#            curr = data[-1]
+#            prev = avg(data[:-1])
+#            return {key: (alpha * curr[key] + (1-alpha) * prev[key] if key not in exceptions else curr[key]) for key in curr.keys()}
+#    return avg(feature_data)
+
+def get_tilt_features(profile, head_rotation, head_translation, landmarks):
+    features = {
+        "measured_tilt_degrees": float(head_rotation[0] - profile.resting_tilt),
+        "lip_spacing": float(landmarks[66][1] - landmarks[62][1] - profile.resting_lip_distance),
+        "eyebrow_elevation": float(landmarks[27][1] - (landmarks[19][1] + landmarks[24][1]) / 2 - profile.resting_eyebrow_elevation)
+    }
+
+    sliding_window.append(features)
+    if len(sliding_window) > WINDOW_LENGTH:
+        sliding_window.pop(0)
+    filtered_features = ewma(sliding_window, set("eyebrow_elevation"))
+    return filtered_features
+
+def get_tilt_weights(profile):
+    return profile.weights
+
+def dot_product(features, weights):
+    return sum([feature_val * weights[feature_label] for feature_label, feature_val in features.items()])
+
+def log_features_weights(features, weights):
+    description = ""
+    for feature_label, feature_val in features.items():
+      description += f"{feature_label}: {feature_val:.3f} * {weights[feature_label]} = {feature_val * weights[feature_label]:.3f} | "
+    description = f"Final Angle: {dot_product(features, weights):.3f}\n" + description
+    print(description)
+
 def ewma(data):
     data.reverse()
     def avg(data):
@@ -227,7 +312,7 @@ def ewma(data):
             return data[0]
         else:
             curr = data[0]
-            print(curr)
+            #print(curr)
             data.pop(0)
             return alpha * curr + (1 - alpha) * avg(data)
     return avg(data)
@@ -242,20 +327,26 @@ def rect_to_coor(rect):
 def detect(frame, mark=False):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = detector(gray)
-    landmarks = np.empty((0, 2), dtype=np.float32)      # landmarks will default to Empty if the detector cannot detect a face, in which case the for-loop below wouldn't run
+    #landmarks, img_points = 0, 0
+    landmarks = np.empty((0, 2), dtype=np.float32)      # landmarks and img_points will default to Empty if the detector cannot detect a face, in which case the for-loops below wouldn't run
     img_points = np.empty((0, 2), dtype=np.float32)
-    for face in faces:      # 'faces' is an iterable of human faces (so with more than one face it will have more than one element)
+    face = faces[0]     # Only operate on the first face detected. If you want to do multiple faces, 'landmarks' and 'img_points' would have to be LISTS of the landmarks and image points for each face!
+
+    if face:
         # Boxing out the faces
         if mark:
             TL, BR = rect_to_coor(face)
-            cv2.rectangle(frame, TL, BR, (0, 255, 0), 3)        # From these two points, we can draw a rectangle
-        
+            cv2.rectangle(frame, TL, BR, (0, 255, 0), 3)        # From these two points, we can draw a 
+
         # Calculating landmarks
         p = predictor(gray, face)
         for i in [17, 21, 22, 26, 36, 39, 42, 45, 31, 35, 48, 54, 57, 8]:       # range(68)   [33, 8, 45, 36, 54, 48]
             x = p.part(i).x
             y = p.part(i).y
-            img_points = np.append(img_points, np.array([[x, y]]), axis=0)
+            test += 1
+            image_points = np.append(img_points, np.array([[x, y]]), axis=0)
+            img_points = image_points
+            print(len(img_points))
             if mark:
                 cv2.circle(frame, (x, y), 3, (255, 0, 0), -1)
         for i in range(68):       # range(68)   [33, 8, 45, 36, 54, 48]
@@ -264,13 +355,21 @@ def detect(frame, mark=False):
             landmarks = np.append(landmarks, np.array([[x, y]]), axis=0)
             if i in range(17, 22) or i in range(22, 27):
                 cv2.circle(frame, (x, y), 3, (0, 255, 0), -1)
-
     return frame, landmarks, img_points
 
 def rescale_frame(frame, percent=100):
     width = int(frame.shape[1] * percent/ 100)
     height = int(frame.shape[0] * percent/ 100)
     return cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
+
+def rotate_marble(tilt, pan):
+    glPushMatrix()
+    glRotatef(tilt, 1, 0, 0)
+    glRotatef(pan, 0, -1, 0)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    draw_sphere()
+    draw_cylinder()
+    glPopMatrix()
 
 
 #############################
@@ -281,12 +380,3 @@ def rescale_frame(frame, percent=100):
 #2. If you create another script import_script.py that IMPORTS THIS SCRIPT using 'import msync':the __name__ variable within import_script.py equals 'msync'
 if __name__ == '__main__':
     main()
-    #m = Thread(main())
-    #m.start()
-    #m.join()
-
-    #t = Thread(marble_renderer.renderer_main())
-    #t.start()
-    #t.join()
-    #marble_renderer.rotate_x = 123
-    #print(marble_renderer.test())
