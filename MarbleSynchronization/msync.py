0758e897dbabb61598b4a552a63caa4bba91c4eb
Jeffrey Tan <tanjeffreyz02@gmail.com>
2020-08-13 21:38:22 -0700

    ready for clean up edits

--- MarbleSynchronization/msync.py
+++ MarbleSynchronization/msync.py
@@ -39,6 +39,8 @@ K_SPEECH = 1.0
 #############################
 #       Initialization      #
 #############################
+np.random.seed(5327)
+
 # Computer Vision Initialization
 detector = dlib.get_frontal_face_detector()     # Get a face detector from the dlib library
 predictor = dlib.shape_predictor('assets/shape_predictor_68_face_landmarks.dat')      # Get a shape predictor based on a trained model from the dlib library
@@ -150,7 +152,7 @@ def main():
             c_n = 30
             rotation_offsets = (0, 0, 0)
 
-            #profile = Profile("Jeffrey")
+            profile = Profile("Jeffrey")
 
             while cap.isOpened():           # Main while loop
                 for event in pygame.event.get():        # Pygame
@@ -178,29 +180,29 @@ def main():
                     
                     #print(img_points)
                     #if len(rotation) != 0:
-                    moving_average.append(rotation)
-                    if len(moving_average) > n:
-                        moving_average.pop(0)
-                    #rotation = [sum([rot[i] for rot in moving_average]) / n for i in range(3)] 
-                    rotation = [ewma([rot[i] for rot in moving_average]) for i in range(3)]
-                    if landmarks.size != 0:
-                        rotation[0] += -K_SPEECH * (landmarks[51][1] - landmarks[57][1] + 20)        # nodding
+                    #moving_average.append(rotation)
+                    #if len(moving_average) > n:
+                    #    moving_average.pop(0)
+                    ##rotation = [sum([rot[i] for rot in moving_average]) / n for i in range(3)] 
+                    #rotation = [ewma([rot[i] for rot in moving_average]) for i in range(3)]
+                    #if landmarks.size != 0:
+                    #    rotation[0] += -K_SPEECH * (landmarks[51][1] - landmarks[57][1] + 20)        # nodding
                     #print(landmarks[51][1] - landmarks[57][1])
                     #print(landmarks[24][1] - landmarks[44][1])
 
-                    g_noise = tuple(numpy.random.normal(scale=3.0) for _ in range(3)) if abs(landmarks[51][1] - landmarks[57][1] + 20) > 5 else (0, 0, 0)
+                    #g_noise = tuple(numpy.random.normal(scale=3.0) for _ in range(3)) if abs(landmarks[51][1] - landmarks[57][1] + 20) > 5 else (0, 0, 0)
 
                     #converted_rotation = tuple(jbr(rot) for rot in rotation)
                     converted_rotation = tuple(rotation[i] + rotation_offsets[i] for i in range(3)) 
 
-                    #tilt_weights = get_tilt_weights(profile)
+                    tilt_weights = get_tilt_weights(profile)
 
-                    #tilt_features = get_tilt_features(profile, rotation, translation, landmarks)
+                    tilt_features = get_tilt_features(profile, converted_rotation, translation, landmarks)
                     
-                    #log_features_weights(tilt_features, tilt_weights)
+                    log_features_weights(tilt_features, tilt_weights)
 
-                    #tilt = dot_product(tilt_features, tilt_weights)
-                    #pan = 0
+                    tilt = dot_product(tilt_features, tilt_weights)
+                    pan = 0
 
                     #rot_frame.append((converted_rotation[0], converted_rotation[1]))
                     #if len(rot_frame) > 2:
@@ -213,10 +215,10 @@ def main():
 
                     #print('timestamp:{3}\npitch:{0}\nyaw:{1}\nroll{2}\n'.format(*converted_rotation, cap.get(cv2.CAP_PROP_POS_MSEC)))
                     #print(rotation_offsets)
-                    file.write('{0},{1},{2}\n'.format(int(cap.get(cv2.CAP_PROP_POS_MSEC)), converted_rotation[0][0], converted_rotation[1][0]))
-
+                    #file.write('{0},{1},{2}\n'.format(int(cap.get(cv2.CAP_PROP_POS_MSEC)), converted_rotation[0][0], converted_rotation[1][0]))
+                    file.write('{0},{1},{2}\n'.format(int(cap.get(cv2.CAP_PROP_POS_MSEC)), tilt, pan))
                     #rotate_marble(pan, tilt)
-                    rotate_marble(converted_rotation[0], converted_rotation[1])
+                    rotate_marble(tilt, pan)
                 else:
                     print('Failed to find image points')
 
@@ -235,23 +237,27 @@ def main():
 #############################
 class Profile:
     def __init__(self, name):
-        if name == "Jaiveer":
+        if name == 'Jaiveer':
             self.resting_tilt = 32
             self.resting_lip_distance = 0
+            self.chatter_lip_distance = 5
             self.resting_eyebrow_elevation = 43
             self.weights = {
-                "measured_tilt_degrees": 1,
-                "lip_spacing": 3,
-                "eyebrow_elevation": -5
+                'measured_tilt_degrees': 1,
+                'lip_spacing': 3,
+                'eyebrow_elevation': -5, 
+                'chatter': 0
             }
-        elif name == "Jeffrey":
+        elif name == 'Jeffrey':
             self.resting_tilt = 20
             self.resting_lip_distance = 2
+            self.chatter_lip_distance = 5
             self.resting_eyebrow_elevation = 32
             self.weights = {
-                "measured_tilt_degrees": 1,
-                "lip_spacing": 3,
-                "eyebrow_elevation": -3
+                'measured_tilt_degrees': 0,
+                'lip_spacing': 0,
+                'eyebrow_elevation': 0,
+                'chatter': 3
             }
 
 
@@ -267,23 +273,25 @@ def create_file(file_name, file_type, n=0):
     else:
         return create_file(file_name, file_type, n+1)
 
-#def ewma(feature_data, exceptions):
-#    def avg(data):
-#        alpha = 2 / (len(data) + 1)
-#        if len(data) == 1:
-#            return data[-1]
-#        else:
-#            curr = data[-1]
-#            prev = avg(data[:-1])
-#            return {key: (alpha * curr[key] + (1-alpha) * prev[key] if key not in exceptions else curr[key]) for key in curr.keys()}
-#    return avg(feature_data)
+def ewma(feature_data, exceptions):
+    def avg(data):
+        alpha = 2 / (len(data) + 1)
+        if len(data) == 1:
+            return data[-1]
+        else:
+            curr = data[-1]
+            prev = avg(data[:-1])
+            return {key: (alpha * curr[key] + (1-alpha) * prev[key] if key not in exceptions else curr[key]) for key in curr.keys()}
+    return avg(feature_data)
 
 def get_tilt_features(profile, head_rotation, head_translation, landmarks):
     features = {
-        "measured_tilt_degrees": float(head_rotation[0] - profile.resting_tilt),
+        "measured_tilt_degrees": float(head_rotation[0]),
         "lip_spacing": float(landmarks[66][1] - landmarks[62][1] - profile.resting_lip_distance),
         "eyebrow_elevation": float(landmarks[27][1] - (landmarks[19][1] + landmarks[24][1]) / 2 - profile.resting_eyebrow_elevation)
     }
+
+    features['chatter'] = np.random.normal() if features['lip_spacing'] > profile.chatter_lip_distance else 0
 
     sliding_window.append(features)
     if len(sliding_window) > WINDOW_LENGTH:
@@ -304,18 +312,18 @@ def log_features_weights(features, weights):
     description = f"Final Angle: {dot_product(features, weights):.3f}\n" + description
     print(description)
 
-def ewma(data):
-    data.reverse()
-    def avg(data):
-        alpha = 2 / (len(data) + 1)
-        if len(data) == 1:
-            return data[0]
-        else:
-            curr = data[0]
-            #print(curr)
-            data.pop(0)
-            return alpha * curr + (1 - alpha) * avg(data)
-    return avg(data)
+#def ewma(data):
+#    data.reverse()
+#    def avg(data):
+#        alpha = 2 / (len(data) + 1)
+#        if len(data) == 1:
+#            return data[0]
+#        else:
+#            curr = data[0]
+#            #print(curr)
+#            data.pop(0)
+#            return alpha * curr + (1 - alpha) * avg(data)
+#    return avg(data)
 
 def rect_to_coor(rect):
     x1 = rect.left()        # These assignments grab the coordinates of the top left and bottom right points of the rectangle[] object
@@ -330,7 +338,8 @@ def detect(frame, mark=False):
     #landmarks, img_points = 0, 0
     landmarks = np.empty((0, 2), dtype=np.float32)      # landmarks and img_points will default to Empty if the detector cannot detect a face, in which case the for-loops below wouldn't run
     img_points = np.empty((0, 2), dtype=np.float32)
-    face = faces[0]     # Only operate on the first face detected. If you want to do multiple faces, 'landmarks' and 'img_points' would have to be LISTS of the landmarks and image points for each face!
+    
+    face = faces[0] if faces else None    # Only operate on the first face detected. If you want to do multiple faces, 'landmarks' and 'img_points' would have to be LISTS of the landmarks and image points for each face!
 
     if face:
         # Boxing out the faces
@@ -343,7 +352,6 @@ def detect(frame, mark=False):
         for i in [17, 21, 22, 26, 36, 39, 42, 45, 31, 35, 48, 54, 57, 8]:       # range(68)   [33, 8, 45, 36, 54, 48]
             x = p.part(i).x
             y = p.part(i).y
-            test += 1
             image_points = np.append(img_points, np.array([[x, y]]), axis=0)
             img_points = image_points
             print(len(img_points))
